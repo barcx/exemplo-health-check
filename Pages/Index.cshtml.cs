59f4e67be3368a4d0cc2b62b682020cd37c9f8b6
Caio Barbosa <caio.barbosa@outlook.com>
2023-05-01 22:18:45 -0300

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HealthCheck.Pages;
+
+public class IndexModel : PageModel
+{
+    public IActionResult OnGet()
+    {
+        return Redirect("~/health");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+using HealthCheck;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddHealthChecks()
+    .AddCheck<SqlHealthCheck>("sql-server", HealthStatus.Unhealthy);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapHealthChecks(
+    "/health",
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
+
+app.Run();
